0292a8f10b7be5cc143497cf1ce04803126747b3
Joel <laeubin.j@gmail.com>
2023-10-25 15:03:34 +0200

    Removed start_http_server, since it is not required

--- app/app.py
+++ app/app.py
@@ -23,5 +23,4 @@ def raise_exception():
     raise ZeroDivisionError
 
 if __name__ == '__main__':
-    start_http_server(8000)  # Start Prometheus metrics server.
     app.run(host='0.0.0.0', port=5000)
